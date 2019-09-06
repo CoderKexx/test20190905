fdc107155a475c08c5e92adb9473ed3434b63327
ke1 <ke1@qq.com>
2019-09-06 17:29:30 +0800

    del abc

--- src/main/java/com/kegc/web/Helloworld.java
+++ src/main/java/com/kegc/web/Helloworld.java
@@ -2,6 +2,4 @@ package com.kegc.web;
 
 public class Helloworld {
 	int a=0;
-	int b=1;
-	int c=2;
 }
--- src/main/java/com/kegc/web/UserController.java
+++ src/main/java/com/kegc/web/UserController.java
@@ -3,8 +3,5 @@ package com.kegc.web;
 public class UserController {
 	public static void main(String[] args) {
 		System.out.println("aaa");
-		System.out.println("bbb");
-		System.out.println("ccc");
-		System.out.println("ddd");
 	}
 }
