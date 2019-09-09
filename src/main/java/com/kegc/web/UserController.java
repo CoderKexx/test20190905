5d6dcb6054972c804713e468d5367e26f4e63283
kegc <kegc@cndatacom.com>
2019-09-09 10:29:43 +0800

    10:29

--- src/main/java/com/kegc/web/UserController.java
+++ src/main/java/com/kegc/web/UserController.java
@@ -2,6 +2,7 @@ package com.kegc.web;
 
 public class UserController {
 	int a=1;
+	boolean flage=true;
 	public static void main(String[] args) {
 		System.out.println("aaa");
 		System.out.println("bbb");
